d6fa96c524ee762b7e43f7908579dfca76216245
will <will@confichannel.com>
2022-06-03 12:33:45 +0200

    Security tip update

--- src/components/SecurityTip.tsx
+++ src/components/SecurityTip.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 export function SecurityTip() {
 	return <div className='mt-2'>
 		<small>
-			💡 ConfiChannel aims to be secure by design but there are things
-			it is dependent on for good security, including: your device, operating
-			system, browser, and browser extensions. To help keep your ConfiChannel
-			messages secure, consider using trusted antivirus software, browsers and
-			extensions, and keep everything up-to-date. Stay safe 🙂
+			💡 ConfiChannel aims to be reasonably secure by design but there are
+			things it is dependent on for good security, including: your device,
+			operating system, browser, browser extensions, and where you share
+			invites. To help keep your ConfiChannel messages secure, consider using
+			trusted antivirus software, browsers and extensions, and keep everything
+			up-to-date. Stay safe 🙂
 		</small>
 	</div>
 }
--- src/pages/TouAgreementPage.tsx
+++ src/pages/TouAgreementPage.tsx
@@ -38,11 +38,12 @@ function TouAgreementPage() {
 				href='/security'>security</AppLink> page.
 		</div>
 		<div className='mt-2'>
-			💡 ConfiChannel aims to be secure by design but there are things
-			it is dependent on for good security, including: your device, operating
-			system, browser, and browser extensions. To help keep your ConfiChannel
-			messages secure, consider using trusted antivirus software, browsers and
-			extensions, and keep everything up-to-date. Stay safe 🙂
+			💡 ConfiChannel aims to be reasonably secure by design but there are
+			things it is dependent on for good security, including: your device,
+			operating system, browser, browser extensions, and where you share
+			invites. To help keep your ConfiChannel messages secure, consider using
+			trusted antivirus software, browsers and extensions, and keep everything
+			up-to-date. Stay safe 🙂
 		</div>
 	</div>
 }
